17318647247bc4527569db00952e8c1a5240fd87
tada-js <justfix010@gmail.com>
2022-05-23 03:33:13 +0900

    :memo: 추가 예제 코드
    추가 튜플 예제 코드

---
+++ example_code/tuple.js
@@ -0,0 +1,3 @@
+"use strict";
+let member = ["김멋사", 10];
+console.log(member);
---
+++ example_code/tuple.ts
@@ -0,0 +1,4 @@
+// let member: [string, number] = ["김멋사", 10];
+// console.log(member);
+
+let member: [string, number] = ["김멋사", 10, 100];
